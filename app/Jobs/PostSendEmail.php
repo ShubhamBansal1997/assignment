d77c23c566f99818b79d2f7e6809113e4f369470
Shubham Bansal <shubhambansal17@hotmail.com>
2021-04-09 02:41:36 +0530

    [feat]: Added Queue job for the email

---
+++ app/Jobs/PostSendEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use Mail;
+use App\Mail\PostEmail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class PostSendEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    protected $details;
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $data = array(
+            "title" => $this->details['title'],
+            "description" => $this->details['description'],
+        );
+        $mail = new PostEmail($data);
+        Mail::to($this->details['email'])->send($mail);
+    }
+}
